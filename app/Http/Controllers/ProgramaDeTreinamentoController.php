36360c816a44f165ac973b8a7bead5c48ab71370
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2019-02-08 14:41:53 -0300

    Criados primeiros Models, Controllers e Migrations

---
+++ app/Http/Controllers/ProgramaDeTreinamentoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProgramaDeTreinamento;
+use Illuminate\Http\Request;
+
+class ProgramaDeTreinamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProgramaDeTreinamento  $programaDeTreinamento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProgramaDeTreinamento $programaDeTreinamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProgramaDeTreinamento  $programaDeTreinamento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProgramaDeTreinamento $programaDeTreinamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProgramaDeTreinamento  $programaDeTreinamento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProgramaDeTreinamento $programaDeTreinamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProgramaDeTreinamento  $programaDeTreinamento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProgramaDeTreinamento $programaDeTreinamento)
+    {
+        //
+    }
+}
---
+++ app/ProgramaExercicios.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramaExercicios extends Model
+{
+    //
+}
