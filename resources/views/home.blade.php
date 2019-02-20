aea79b9fee6580b2d33ffc6ffd6c055627d94f7f
Lidiane <lidianeneves79@gmail.com>
2019-02-20 01:23:04 -0300

    criando estrutura da home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,141 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- O menu esta dentro da pasta layout, nome do aquivo main-->
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+
+<!--Decricacao da academia-->
+
+<div class="jumbotron jumbotron-fluid text-center">
+  <div class="container">
+    <img src="..." class="img-fluid" alt="Responsive image">
+    <h1 class="display-4">ACADEMIA FITNESS</h1>
+    <p class="lead">DESCRIÇÃO This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
 </div>
+</header>
+
+<!--texto modalidades-->
+<div class="container">
+        <div class="section-header">
+          <h2 class="text-center">MODALIDADES</h2>
+        </div>
+
+<!--tipos de modalidades-->
+
+<div class="container">
+  <div class="card-deck "  >
+  <div class="row">
+    <div class="col-sm-4 card " style="border:none;"  >
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+    <div class="col-sm-4 card"  style="border:none;">
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+    <div class="col-sm-4 card" style="border:none;" >
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+  </div>
+</div>
+
+<div class="card-deck" >
+  <div class="row">
+    <div class="col-sm-4 card"  style="border:none;">
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+    <div class="col-sm-4 card"  style="border:none;">
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+    <div class="col-sm-4 card"  style="border:none;">
+      <img class="card-img-top" src="img/musculacao.png" alt="Imagem de capa do card">
+      <h3 class="text-center">musculação</h3>
+    </div>
+  </div>
+</div>
+</div>
+
+<br>
+
+<!-- chamada para planos -->
+<div class="container">
+<div class="row">
+  <div class="col-sm-12">
+    <div class="row">
+      <div class="col-12 col-sm-6">
+        <h3>PLANOS PERFEITOS PARA VOCÊ</h3>
+        <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
+      </div>
+      <div class="col-12 col-sm-6">
+        <img src="img/musculacao.png" class="img-fluid" alt="Imagem responsiva">
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<br>
+
+
+<!--tipos de planos-->
+<div class="container"> 
+<div class="card-deck">
+  <div class="card bg-primary" style="border:none;">
+    <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+      <p class="card-text">Icones e valor</p>
+    </div>
+  </div>
+  <div class="card bg-primary" style="border:none;">
+    <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+    </div>
+  </div>
+  <div class="card bg-primary" style="border:none;">
+   <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+    </div>
+  </div>
+</div>
+<br>
+<div class="card-deck">
+  <div class="card bg-primary" style="border:none;">
+    <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+      <p class="card-text">Icones e valor</p>
+    </div>
+  </div>
+  <div class="card bg-primary" style="border:none;">
+    <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+    </div>
+  </div>
+  <div class="card bg-primary" style="border:none;">
+   <div class="card-body text-center">
+      <h5 class="card-title">MUSCULAÇÃO</h5>
+      <p class="card-text">Primeira mensalidade:</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<br>
+
+<!-- O foooter esta dentro da pasta layout, nome do aquivo main-->
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,36 +2,58 @@
 <head>
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <title>Academia Fitness</title>
 </head>
 <body>
-  <div class="container">
+ 	<header>
+ 		<div class="continer">
+ 			<nav class="navbar navbar-expand-lg navbar-light bg-light" >
+  <!--logo -->
+  <a class="navbar-brand" href="#"><img src="">Logo</a>
 
-
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
-		<div class="container">
-     
-		  <a class="navbar-brand h1 mb-0" href="/">Academia Fitness</a>
-	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
-	          <span class="navbar-toggler-icon"></span>
-	      </button>
-
-	      <div class="collapse navbar-collapse" id="navbarSite">
-	      	
-			  <ul class="navbar-nav ml-auto">
-			    <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
-			    <li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>
-			    <li class="nav-item"><a class="nav-link" href="/register">Cadastrar-se</a></li>
-			  </ul>
-	      </div>
-		</div>
-
-	</nav>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!--links -->
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link" href="site/perfilaluno.blade.php">Sobre nós</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    
+ 		</div>
+ 
 
     @yield('content')
 
+
+<div class="container">
+<div class="row">
+  <div class="col-sm-12">
+    <div class="row">
+      <div class="col-12 col-sm-4">
+        <p>REDES SOCIAS</p>
+      </div>
+      <div class="col-12 col-sm-4">
+        <P>ONDE NOS ENCONTRAR?</P>
+        <i>Rua São José, 1143</i>
+        <i>Proximo a escola Pudium</i>
+      </div>
+      <div class="col-12 col-sm-4">
+        <P>CONTATO</P>
+        <i>(00) 0000-000</i>
+      </div>
+    </div>
   </div>
+</div>
+</div>
 
   <script type="text/javascript" src="/js/app.js"></script>
   @stack('scripts')
