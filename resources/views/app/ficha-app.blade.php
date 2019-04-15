a3a0b831fa5534e3c2461404ef2a86858537ada0
Lidianenevez <lidianeneves79@gmail.com>
2019-04-15 04:55:41 -0300

    criando ficha app

--- resources/views/app/ficha-app.blade.php
+++ resources/views/app/ficha-app.blade.php
@@ -1,21 +1,27 @@
 @extends('layouts.main')
 
 @section('content')
-<nav class="nav nav-pills nav-fill">
-  <a class="nav-item nav-link" href="/perfil-app" ><i class="fa fa-user-o" aria-hidden="true" ></i></a>
-  <a class="nav-item nav-link" href="/ficha-app"><i class="fa fa-file-text-o " aria-hidden="true"></i></a>
+<br><br>
+<!-- Just an image -->
+<nav class="navbar navbar-light colormenu">
+    <a class="navbar-brand" href="/"><img src="../img/logo.png" style="max-width:200px; max-height: 50px"></a>
+  <a class="navbar-brand" href="#">
+    <a href="#" class="btn btnlaranja" style="color: #F1F1F1; border-radius: 20px; ">Sair <i class="fa fa-share-square-o" aria-hidden="true"></i></a>
+  </a>
 </nav>
-
-      <div class="container">
+<nav class="nav nav-pills nav-fill colormenu fixed-bottom">
+  <a class="nav-item nav-link link-nav" href="/perfil-app" ><i class="fa fa-user-o "style="color: #F1F1F1" aria-hidden="true" ></i></a>
+  <a class="nav-item nav-link link-nav" href="/ficha-app"><i class="fa fa-file-text-o" style="color: #F1F1F1" aria-hidden="true"></i></a>
+</nav>
+ <div class="container">
         <div class="section-header">
           <h2 class="text-center">MINHA FICHA</h2>
           <hr style="max-width: 122px; height: 3px; background-color: #B95922;">
         </div>
-        <h2>Treino de Hoje: <i class="fa fa-hand-o-right playicon" aria-hidden="true" ></i></h2>
+        <h2>Treino de Hoje: <i class="fa fa-hand-o-right playicon" aria-hidden="true" ></i> B</h2>
       </div>
 
-
-      <div class="container justify-content-center ">
+<div class="container justify-content-center ">
     <div class="row">
       <div class="col-12 col-sm-6">
         <form class="form-inline">
@@ -41,17 +47,14 @@
               <p class="nomealuno" style="font-size: 20px">Sequência de treinos: </p>
            <p class="">
       
-              <span class="fontvigor" style="font-size: 25px">  <i class="fa fa-hand-o-right playicon" aria-hidden="true"></i> 
+              <span class="fontvigor" style="font-size: 25px"> A  <i class="fa fa-hand-o-right playicon" aria-hidden="true"></i> B
              
              e repete!</span>
           </p>
         </div>
         </div>
       </div>
-        
-
-
-    <div class="container justify-content-center ">
+<div class="container justify-content-center ">
     <div class="row">
       <div class="col-12 col-sm-6">
         <div class="table-responsive">
@@ -64,8 +67,8 @@
   </thead>
   <tbody>
     <tr>
-      <td scope="row" class="ficha" > </td>
-      <td class="ficha"> </td>
+      <td scope="row" class="ficha" >Emagrecer </td>
+      <td class="ficha"> Normal</td>
     </tr>
   </tbody>
 </table>
@@ -83,8 +86,8 @@
   </thead>
   <tbody>
     <tr>
-      <td scope="row" class="ficha" > minutos <i class="fa fa-clock-o playicon" aria-hidden="true "></i></td>
-      <td class="ficha"> segundos <i class="fa fa-clock-o playicon" aria-hidden="true"></i></td>
+      <td scope="row" class="ficha" > 10 minutos <i class="fa fa-clock-o playicon" aria-hidden="true "></i></td>
+      <td class="ficha"> 30 segundos <i class="fa fa-clock-o playicon" aria-hidden="true"></i></td>
       
     </tr>
   </tbody>
@@ -94,33 +97,56 @@
 </div>
 </div>
 
+
+<div class="container">
+<div class="card mb-3 text-white " style="max-width: 540px; border-radius: 20px; border:none; background: #9EDEF366">
+  <div class="row">
+    <div class="col-4 text-center">
+       <button type="button" class="btn" data-toggle="modal" data-target=".bd-example-modal-xl" style="border-radius: 20%; background-color: #fff0">
+                <img src="../img/play.png" style="max-width: 90px; margin-top:15px;">
+                
+              </button>
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title text-left ficha" >Agachamento Livre</h5>
+        <p class="card-text ficha" style="font-size:16px">SÉRIE: <strong>4 X 20</strong></p>
+        <p class="card-text ficha" style="font-size:16px">PESO: <strong>40kg</strong> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="card mb-3 text-white " style="max-width: 540px; border-radius: 20px; border:none; background: #9EDEF366">
+  <div class="row">
+    <div class="col-4 text-center">
+       <button type="button" class="btn" data-toggle="modal" data-target=".bd-example-modal-xl" style="border-radius: 20%; background-color: #fff0">
+                <img src="../img/play.png" style="max-width: 90px; margin-top:15px;">
+                
+              </button>
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title text-left ficha" >Agachamento Livre</h5>
+        <p class="card-text ficha" style="font-size:16px">SÉRIE: <strong>4 X 20</strong></p>
+        <p class="card-text ficha" style="font-size:16px">PESO: <strong>40kg</strong> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+     
+
+    
  <div class="container" >
 <div class="row " >
       <div class="col-12 col-sm-12 ">
         <div class=" " style="min-width: -50em">
   <table class="table table-sm table-bordered" >
-  <thead class="text-center fontth">
-    <tr class="bg-primary">
-      <th scope="col">EXERCÍCIO</th>
-      <th scope="col">SÉRIE</th>
-      <th scope="col">PESO</th>
-    </tr>
-  </thead>
-  <tbody class="fonttd">
-     
-        <tr>
-          <td scope="row"> <input type="checkbox" aria-label="Checkbox for following text input" style="border: 1px solid #ccc"> 
-              <!-- Button trigger modal -->
-              <button type="button" class="btn" data-toggle="modal" data-target=".bd-example-modal-xl" style="border-radius: 20%">
-                <i class="fa fa-play-circle-o playicon" aria-hidden="true" style="font-size: 25px"></i>
-              </button>
-
-            </td>
-          <td class="text-center"></td>
-          <td class="text-center">Kg</td>
-        </tr>
-  
-  </tbody>
   <tfoot class="fonttd">
     <tr>
       <th scope="col" colspan="3">
@@ -138,7 +164,7 @@
     </tr>
     <tr>
       <th scope="col">
-       
+       Data: 25/02/2019
       </th>
        <th scope="col" colspan="2">
         Revisão: 25/02/2019
