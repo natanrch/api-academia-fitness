a0eca551b9adbf4637be04060ae6a1250801bb4c
Lidianenevez <lidianeneves79@gmail.com>
2019-05-18 16:32:26 -0300

    img homr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,10 @@
           
         </div>
          <div class="col-sm-7 col-12 align-content-sm-center">
-          <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="img/video.mp4" allowfullscreen></iframe>
+          <img src="../img/video.jpeg" class="img-fluid">
+         <!-- <div class="embed-responsive embed-responsive-16by9">
+            
+        <iframe class="embed-responsive-item" src="img/video.mp4" allowfullscreen></iframe> -->
       </div>
       <div class="text-center">
             <a href="" class="btn "><img src="img/store.png" alt="baixa aplicativo na play store" class="img-fluid" style="max-width: 200px; max-height: 50px" ></a>
